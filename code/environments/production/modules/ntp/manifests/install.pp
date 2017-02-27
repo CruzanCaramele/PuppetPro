95c9d06dd2862e345bcc396e1e25ce7110279dfb
CruzanCaramele <letters2hamza@gmail.com>
2017-02-27 21:31:20 +0800

    install ntp package

--- code/environments/production/modules/ntp/manifests/install.pp
+++ code/environments/production/modules/ntp/manifests/install.pp
@@ -11,6 +11,21 @@ class ntp::install {
 				ensure => installed,
 			}
 		}
+		'Windows': {
+			# code
+			file { 'c:\\ntp-4.2.8p7-win32-setup.exe':
+				ensure => file,
+				source => 'puppet:///modules/ntp/meinberg-ntp.4.2.8.nupkg',
+			}
+
+			package { 'meinberg-ntp':
+				ensure   => latest,
+				provider => chocolatey,
+				source   => "c:\\meinberg-ntp.4.2.8.nupkg",
+				require  => File['c:\\meinberg-ntp.4.2.8.nupkg'],
+			}
+		}
+
 		default: {
 			# code
 			fail("Sorry, ${::kernel} is unspported by this ntp module")
